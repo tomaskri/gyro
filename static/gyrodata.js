6f071ccd2b57d1b737ab4ecebce4575192bcabd7
tomaskri <98184013+tomaskri@users.noreply.github.com>
2023-01-02 23:46:58 +0000

    changes

--- static/gyrodata.js
+++ static/gyrodata.js
@@ -1,7 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
   const texti = document.querySelector('.pee');
   texti.innerHTML = "world";
-  
+
+    const accX = document.querySelector('#accX');
+    const accY = document.querySelector('#accY');
+    const accZ = document.querySelector('#accZ');
+    const gyroX = document.querySelector('#gyroX');
+    const gyroY = document.querySelector('#gyroY');
+    const gyroZ = document.querySelector('#gyroZ');
+    const oriX = document.querySelector('#oriX');
+    const oriY = document.querySelector('#oriY');
+    const oriZ = document.querySelector('#oriZ');
+
+
   function sendData(datum){
     fetch('/process_data', {
       method: 'POST',
@@ -26,81 +37,45 @@ document.addEventListener('DOMContentLoaded', function() {
       typeof DeviceMotionEvent.requestPermission === "function"
     ) {
       sendData("Requesting data...");
-      DeviceOrientationEvent.requestPermission().then(permissionState => {sendData(permissionState)});
+      DeviceOrientationEvent.requestPermission().then(permissionState => {
+          window.addEventListener('devicemotion', function(event) {
+            var acceleration = event.acceleration;
+            var rotationRate = event.rotationRate;
+            accX.innerHTML = acceleration.x;
+            accY.innerHTML = acceleration.y;
+            accZ.innerHTML = acceleration.z;
+            gyroX.innerHTML = rotationRate.beta;
+            gyroY.innerHTML = rotationRate.gamma;
+            gyroZ.innerHTML = rotationRate.alpha;
+            //sendData(acceleration);
+            //console.log('Acceleration:', acceleration);
+          });
+          window.addEventListener('deviceorientation', function(event) {
+            var alpha = event.alpha;
+            var beta = event.beta;
+            var gamma = event.gamma;
+            oriX.innerHTML = gamma;
+            oriY.innerHTML = beta;
+            oriZ.innerHTML = alpha;
+            //sendData(acceleration);
+          });
+
+
+      });
+
+
     }
-    // DeviceMotionEvent.requestPermission()
-    //   .then(response => {
-    //     if (response == 'granted') {
-    //       // Permission granted, register the event listener
-    //       window.addEventListener('devicemotion', function(event) {
-    //         var acceleration = event.acceleration;
-    //         texti.innerHTML = acceleration; 
-    //         sendData(acceleration);
-    //         console.log('Acceleration:', acceleration);
-    //       });
-    //     }
-    //   })
 
 
 
 
 
-    // if (typeof DeviceMotionEvent.requestPermission === 'function') {
-
-    //   sendData("supported")
-    //   DeviceMotionEvent.requestPermission()
-    //     .then(permissionState => {
-    //       if (permissionState === 'granted') {
-    //         sendData("granted")
-    //         window.addEventListener('devicemotion', function(event) {
-    //           var acceleration = event.acceleration;
-    //           texti.innerHTML = acceleration; 
-    //           sendData(acceleration);
-    //           console.log('Acceleration:', acceleration);
-    //         });
-    //       }
-    //       else{
-    //         sendData("Denied");
-    //         sendData(permissionState);
-    //       }
-    //     })
-    //     .catch(console.error);
-    // } else {
-    //   sendData("some error")
-    //   console.warn('DeviceMotionEvent.requestPermission is not supported on your device');
-    // }
 
 
 
 
     });
-    
-
-  
 
 
-  // window.addEventListener('devicemotion', function(event) {
-  //   var acceleration = event.acceleration;
-  //   texti.innerHTML = acceleration; 
-  //   sendData(acceleration);
-  //   console.log('Acceleration:', acceleration);
-  // });
 
-    // if ('ondevicemotion' in window) {
-    //   DeviceMotionEvent.requestPermission()
-    //     .then(response => {
-    //       if (response == 'granted') {
-    //         // Permission granted, register the event listener
-    //         window.addEventListener('devicemotion', function(event) {
-    //           // Your code here
-    //         });
-    //       } else {
-    //         // Permission denied, provide a fallback or alternative solution
-    //       }
-    //     })
-    //     .catch(console.error);
-    // } else {
-    //   // Device does not support the devicemotion event, provide a fallback or alternative solution
-    // }
-    
 });
