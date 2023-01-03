79be924efa37b6d3448ff79088356962c6a4b878
tomaskri <98184013+tomaskri@users.noreply.github.com>
2023-01-03 02:36:33 +0000

    pos kinda workin

--- app.py
+++ app.py
@@ -2,15 +2,30 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, request, render_template, redirect
+import numpy as np
 #from flask_sqlalchemy import SQLAlchemy
 import json
 
+with open("trash.log", "a") as f:
+    f.writelines("TEST, started\n")
 
-app = Flask(__name__, template_folder='/home/tomaskri/mysite/templates/', static_folder='/home/tomaskri/mysite/static/')
+app = Flask(__name__) #, template_folder='/home/tomaskri/mysite/templates/', static_folder='/home/tomaskri/mysite/static/'
 
 @app.route('/')
 def index():
     return render_template('index.html') #return 'Hello from Flask!'
+
+button_active = False
+@app.route('/press_button', methods=['POST'])
+def press_button():
+    global button_active
+    button_active = not button_active
+    if button_active:
+        global position, velocity, orientation
+        position = np.array([1.5, 0.0, 0.0]).astype(float)
+        velocity = np.array([0.0, 0.0, 0.0]).astype(float)
+        orientation = np.array([0.0, 0.0, 0.0]).astype(float)
+    return json.dumps({"data":"return"}) #þetta gefur bara error á 'r' eins og hitt á 'p'  en já sé errorinn, nei þetta er betri error, því hann er bara svona warning
 
 
 @app.route('/process_data', methods=['POST'])
@@ -19,4 +34,85 @@ def process_data():
         print(json.loads(request.get_data())['data'], flush=True)
 
     # Process the data and return a response
-    return "pee"
+    return json.dumps({"data":"return"}) #enginn error :) 
+
+# Initialize position, velocity, and orientation
+position = np.array([0.0, 0.0, 0.0]).astype(float)
+velocity = np.array([0.0, 0.0, 0.0]).astype(float)
+orientation = np.array([0.0, 0.0, 0.0]).astype(float)
+
+@app.route('/update_ori', methods=['POST'])
+def update_ori():
+    ori = json.loads(request.get_data())["data"]
+    global orientation
+    orientation[:] = ori
+    pass
+    return json.dumps({"data":"return"}) #enginn error :) 
+
+@app.route('/update_pos', methods=['POST'])
+def update_pos():
+    with open("trash.log", "a") as f:
+        f.writelines("Running update_pos()...\n")
+        # print("Running update_pos()...", file=f)
+    print(f"Running update_pos()...")
+    data = json.loads(request.get_data())
+    accel, interval = data["accel"], data["interval"]
+    update_position([0.0,0.0,0.0], accel, interval)
+    print(position, velocity)
+    return json.dumps({"data":"return"}) #enginn error :)
+
+@app.route('/get_position', methods=['GET'])
+def get_position():
+    global position, velocity
+    with open("trash.log", "a") as f:
+        f.write(f"position: {position},  velocity: {velocity}\n")
+    return json.dumps({"data": [float(position[0]), float(position[1]), float(position[2])]})
+    
+
+def update_position(gyroscope, accelerometer, time_interval):
+    global position, velocity, orientation
+
+    # Iterate over the sensor data and update position, velocity, and orientation
+    # for i in range(len(gyroscope)):
+    gyro_data = np.array(gyroscope)
+    accel_data = np.array(accelerometer)
+    dt = time_interval
+    
+    # Update orientation using gyroscope data
+    orientation += gyro_data * dt
+    
+    # Transform accelerometer data into global coordinate system
+    R = rotation_matrix(orientation)
+    accel_global = np.matmul(R, accel_data)
+    with open("trash.log", "a") as f:
+        f.writelines(f"accel_global: {accel_global}\n")
+    
+    # Update velocity using accelerometer data
+    velocity += accel_global * dt
+    
+    # Update position using velocity data
+    position += velocity * dt
+    
+    return position, velocity, orientation
+
+
+def rotation_matrix(orientation):
+    # Convert orientation angles to radians
+    roll = np.radians(orientation[0])
+    pitch = np.radians(orientation[1])
+    yaw = np.radians(orientation[2])
+    
+    # Create rotation matrix using Euler angles
+    R_x = np.array([[1.0, 0.0, 0.0],
+                    [0.0, np.cos(roll), -np.sin(roll)],
+                    [0.0, np.sin(roll), np.cos(roll)]])
+    R_y = np.array([[np.cos(pitch), 0.0, np.sin(pitch)],
+                    [0.0, 1.0, 0.0],
+                    [-np.sin(pitch), 0.0, np.cos(pitch)]])
+    R_z = np.array([[np.cos(yaw), -np.sin(yaw), 0.0],
+                    [np.sin(yaw), np.cos(yaw), 0.0],
+                    [0.0, 0.0, 1.0]])
+                    
+    R = R_z @ R_y @ R_x
+    return R
+    
--- static/gyrodata.js
+++ static/gyrodata.js
@@ -5,12 +5,16 @@ document.addEventListener('DOMContentLoaded', function() {
     const accX = document.querySelector('#accX');
     const accY = document.querySelector('#accY');
     const accZ = document.querySelector('#accZ');
+    const interval = document.querySelector('#interval');
     const gyroX = document.querySelector('#gyroX');
     const gyroY = document.querySelector('#gyroY');
     const gyroZ = document.querySelector('#gyroZ');
     const oriX = document.querySelector('#oriX');
     const oriY = document.querySelector('#oriY');
     const oriZ = document.querySelector('#oriZ');
+    const posX = document.querySelector('#posX');
+    const posY = document.querySelector('#posY');
+    const posZ = document.querySelector('#posZ');
 
 
   function sendData(datum){
@@ -21,16 +25,72 @@ document.addEventListener('DOMContentLoaded', function() {
         'Content-Type': 'application/x-www-form-urlencoded'
       }
     })
-      .then(response => response.json())
+      // .then(response => response.json())
       // .then(data => {
       //   // Handle the response from the server
       // });
     }
+  
+  function press_button(){
+    fetch('/press_button', {
+      method: 'POST',
+      body: JSON.stringify({}),
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    })
+  }
+
+  function update_ori(ori){
+    fetch('/update_ori', {
+      method: 'POST',
+      body: JSON.stringify({data: ori}),
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    })
+  }
+  
+  function update_pos(accel, inter){
+    fetch('/update_pos', {
+      method: 'POST',
+      body: JSON.stringify({"accel": accel, "interval":inter}),
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    })
+  }
+
+  function get_pos(){
+    // fetch('/get_position', {
+    //   method: 'GET',
+    //   body: JSON.stringify({}),
+    //   headers: {
+    //     'Content-Type': 'application/x-www-form-urlencoded'
+    //   }
+    // })
+    fetch('/get_position')
+    .then(response => response.json())
+    .then(data => {
+      // Handle the response from the server
+      sendData(data);
+      data = data["data"]
+      // console.log(data);
+      // console.log(data[0]);
+      // console.log(data[0].toString())
+      posX.innerHTML = data[0].toString();
+      posY.innerHTML = data[1].toString();
+      posZ.innerHTML = data[2].toString();
+    });
+  }
 
   document.querySelector('#submit').addEventListener('click', function(e) {
 
     e.preventDefault();
     sendData("I sent data")
+    get_pos()
+    update_pos([0.1, 0.12, 0.2], 0.16)
+    press_button()
     // Request permission for iOS 13+ devices
     if (
       DeviceMotionEvent &&
@@ -47,16 +107,20 @@ document.addEventListener('DOMContentLoaded', function() {
             gyroX.innerHTML = rotationRate.beta;
             gyroY.innerHTML = rotationRate.gamma;
             gyroZ.innerHTML = rotationRate.alpha;
+            interval.innerHTML = event.interval;
             //sendData(acceleration);
             //console.log('Acceleration:', acceleration);
+            update_pos([acceleration.x, acceleration.y, acceleration.z], event.interval)
+            get_pos()
           });
           window.addEventListener('deviceorientation', function(event) {
             var alpha = event.alpha;
             var beta = event.beta;
             var gamma = event.gamma;
-            oriX.innerHTML = gamma;
-            oriY.innerHTML = beta;
+            oriX.innerHTML = beta;
+            oriY.innerHTML = gamma;
             oriZ.innerHTML = alpha;
+            update_ori([beta, gamma, alpha])
             //sendData(acceleration);
           });
 
